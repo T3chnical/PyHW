076ce173a474f9940585471dc1aea4dac4d5552b
your-name <your-email>
2021-01-10 22:03:35 -0500

    Sync Upstream + Name

--- Homework/HW4.py
+++ Homework/HW4.py
@@ -1,4 +1,4 @@
-# YOUR NAME HERE
+# Louis
 
 """
 Problem 1
--- Scripts/sync-upstream.ps1
+++ Scripts/sync-upstream.ps1
@@ -1,5 +1,5 @@
-git config --global user.name 'your-name'
-git config --global user.email 'your-email'
+git config --global user.name 'T3chnical'
+git config --global user.email 'thisnewacc46@gmail.com'
 
 git pull --unshallow
 
